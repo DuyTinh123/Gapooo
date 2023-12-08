daf722d26efc3343d8b4b5749c28aed65ab10abb
DuyTinh252 <hoangyen250201@gmail.com>
2023-12-09 00:49:11 +0700

    Hoàn thành cài đặt và tạo font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import './App.css';
-import { useState } from 'react';
-
-
-
+import { Fragment } from 'react'; // chỉ là để tạo thẻ chưa
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/Routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return(
-    <h1>hello ae</h1>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Routes/index.js
@@ -0,0 +1,19 @@
+//layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Seach from '~/pages/Seach';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/Seach', component: Seach, layout: null },
+];
+
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
